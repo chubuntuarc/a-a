9433fb0c3b2785cf405ecac99597e8c26305d7fa
Jesus Arciniega <dev.jesusarc@gmail.com>
2018-10-01 01:56:58 -0600

    Datatables actualizado

--- public/js/familias.js
+++ public/js/familias.js
@@ -41,16 +41,19 @@ function leerDatosFamilia(){
   familias.on('value',function(snap){
     $("#familias-rows > tr").remove()
     var datos = snap.val()
+     var nuevaFila
     for(var key in datos){
-      var nuevaFila='<tr>'
+          nuevaFila+='<tr>'
           nuevaFila+='<td>'+datos[key].nombre+'</td>'
           nuevaFila+='<td><a href="#!" onclick="editarFamilia(\''+key+'\');"><i class="material-icons">edit</i></a></td>'
-          nuevaFila+='<td><a href="#!" onclick="borrarFamilia(\''+key+'\');"><i class="material-icons">delete</i></a></td>'
+          //nuevaFila+='<td><a href="#!" onclick="borrarFamilia(\''+key+'\');"><i class="material-icons">delete</i></a></td>'
+          nuevaFila+='<td class="hide-on-small-only"><a class="red-text text-lighten-3" href="#!" onclick="borrarFamilia(\''+key+'\');"><i class="tiny material-icons">clear</i></a></td>'
           nuevaFila+='</tr>'
-          $("#familias-rows").append(nuevaFila)
+          
     }
+    $("#familias-rows").append(nuevaFila)
+    datatable()
   })
-  //$('#condominios-rows').fadeIn().delay(2000);
 }
 
 function borrarFamilia(key){
